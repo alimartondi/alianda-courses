5455cf1ea79bb311f822f8340ca5198513dd6ffb
ali <alimartondi88@gmail.com>
2020-11-18 23:31:41 +0700

    Improve register card background image

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import Register from "../images/register.png";
 import "../style/style.css";
+import Shape from "../images/shape-2.png";
 
 export default function RegisterSection() {
   return (
@@ -9,12 +10,16 @@ export default function RegisterSection() {
       <Container>
         <Row>
           <Col lg={12}>
-            <div className="register-card d-block d-md-flex align-items-center ">
-              <div className="register-body text-center  mr-0 mr-xl-5 mt-5">
+            <div className="register-card d-block d-md-flex align-items-center justify-content-between">
+              <div className="register-body text-center text-md-left mr-0 mr-xl-5 mt-5">
                 <h3 className="second-title">
                   Daftar Gratis untuk Ikuti Semua Program WISH!
                 </h3>
-                <Button variant="info mt-5">DAFTAR SEKARANG</Button>
+                <p className="subtitle mt-3">
+                  Dapatkan kesempatan untuk mengikuti berbagai workshop
+                  persiapan beasiswa
+                </p>
+                <Button variant="info mt-3">Daftar Sekarang</Button>
               </div>
               <div className="register-card-img">
                 <img
